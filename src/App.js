ca71a72015c357af500de13796f78297cf9c395c
Matheus Ramos <matheus.ramos.silva.santos@gmail.com>
2020-06-27 21:57:03 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 import "./styles.css";
 
-function App() {
+ function App() {
+
+  const [ repositories, setRepositories ] = useState([])
+
+  useEffect(() => {//sempre que algo acontece ele roda esse trecho de código
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+
+  // async function handleAddRepository() {
+  //  api.post('repositories', {
+  //     title: `New repository, ${Date.now()}`,
+  //     url: `http://github.coom/repository/${Date.now()}`,
+  //     techs: [
+  //       "NodeJS",
+  //       "ReactJS",
+  //       "ReactNative"
+  //     ]
+  //   }).then((response) => {
+  //     setRepositories([...repositories, response.data])
+  //   } )
+    
+  // }
+
   async function handleAddRepository() {
-    // TODO
-  }
+    const response = await api.post('repositories', {
+       title: `New repository, ${Date.now()}`,
+       url: `http://github.coom/repository/${Date.now()}`,
+       techs: [
+         "NodeJS",
+         "ReactJS",
+         "ReactNative"
+       ]
+     });
+      setRepositories([...repositories, response.data])
+     
+   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+   await api.delete(`repositories/${id}`)
+   
+    setRepositories(repositories.filter(
+
+      repository => repository.id !== id //faz um filtro no array repositories, procura quando o id encontrado for diferente o id que vem na função e insere esses valores no setRepositories
+ 
+      ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        { repositories.map(repository => <li key={repository.id}>
+          {repository.title}          
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li> )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
